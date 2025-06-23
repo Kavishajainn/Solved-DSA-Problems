e195b3999a9b6251f670bf932d962fa95caaab48
Kavisha Jain <kavishajain66@gmail.com>
2025-06-23 11:51:06 +0530

    init commit

---
+++ src/Arrays/A17_checkArrayIsSorted.java
@@ -0,0 +1,29 @@
+/*
+* problem-- check if an array is sorted
+* link-- https://takeuforward.org/data-structure/check-if-an-array-is-sorted/
+* time complexity-- O(n)*/
+
+package Arrays;
+
+import java.util.Scanner;
+
+public class A17_checkArrayIsSorted {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the length of array: ");
+        int a=sc.nextInt();
+        int[] arr=new int[a];
+        System.out.println("enter the value of array: ");
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        //check if array is sorted
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<arr[i+1]){
+                System.out.println("not sorted!");
+                System.exit(0);
+            }
+        }
+        System.out.println("array is sorted! ");
+    }
+}
