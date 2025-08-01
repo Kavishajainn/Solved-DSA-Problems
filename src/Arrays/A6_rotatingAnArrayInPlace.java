853b30f63880475d64e5b612d0565363d2d1fcac
Kavisha Jain <kavishajain66@gmail.com>
2025-08-01 10:47:46 +0530

    init commit

--- src/Arrays/A6_rotatingAnArrayInPlace.java
+++ src/Arrays/A6_rotatingAnArrayInPlace.java
@@ -1,4 +1,26 @@
 package Arrays;
 
 public class A6_rotatingAnArrayInPlace {
+    public static void reverse(int[] arr, int start, int end){
+        while(end>start){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+    public static void rotate(int[] arr,int d){
+        reverse(arr,0,d-1);
+        reverse(arr,d,arr.length-1);
+        reverse(arr,0, arr.length-1);
+    }
+    public static void main(String[] args) {
+        int d=2;
+        int[] arr={1,2,3,4,5};
+        rotate(arr,d);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+", ");
+        }
+    }
 }
